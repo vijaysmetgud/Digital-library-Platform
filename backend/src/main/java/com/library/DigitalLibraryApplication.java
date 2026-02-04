70976f7427486f3002fe39a2bef41d2efdb41514
vijay <vsmetgud@gmail.com>
2026-02-04 17:34:52 +0530

    maven is added in backend src java

---
+++ backend/src/main/java/com/library/DigitalLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalLibraryApplication.class, args);
+    }
+}
